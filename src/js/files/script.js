e49a01f4555c10dddecd7686c94a77af3d54794f
unknown <krutko7779@gmail.com>
2024-02-12 13:14:48 +0300

    corrected prices

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,20 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+// Отключение transition до полной загрузки страницы
+if ( document.querySelector('.button-offer') ) {  //
+	let elem = document.querySelector('.button-offer'); 
+
+			window.onload = function() {
+			elem.classList.remove('preload');
+		};	
+}
+
+  // Кнопка НАЗАД
+if ( document.querySelector('.button-back') ) {  // Проверяем наличие элемента на странице
+	document.querySelector('.button-back').onclick = function(){ // Клик по кнопке НАЗАД
+		window.history.go(-1); return false;// возвращаемся назад
+	};
+}
+
